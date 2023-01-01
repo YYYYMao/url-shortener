3256fcaf1c1aec00fb5db09139918964a52b9017
Ying Mao <ying.mao@wemoscooter.com>
2023-01-02 03:16:21 +0800

    tests: add test

--- repositories/urls.go
+++ repositories/urls.go
@@ -1,40 +1,57 @@
 package repo
 
 import (
-	"fmt"
+	"errors"
 	"time"
 	"urlshortener/repositories/model"
-	"urlshortener/utils/pg"
+
+	"gorm.io/gorm"
 )
 
 var fields = []string{"url_id", "url", "expire_at"}
 
-func SelectUrl(urlId string) (*model.Urls, error) {
+type urlRepository struct {
+	db *gorm.DB
+}
+
+func NewUrlRepository(db *gorm.DB) UrlRepository {
+	return &urlRepository{
+		db: db,
+	}
+}
+
+type UrlRepository interface {
+	SelectByUrlId(urlId string) (*model.Urls, error)
+	Create(urlId string, url string, expireAt time.Time) error
+	IsUrlIdExist(urlId string) bool
+}
+
+func (r *urlRepository) SelectByUrlId(urlId string) (*model.Urls, error) {
 	url := &model.Urls{}
-	if err := pg.Db.Select(fields).Where("url_id=?", urlId).First(&url); err.Error != nil {
+	if err := r.db.Select(fields).Where("url_id=?", urlId).First(&url); err.Error != nil {
 		return nil, err.Error
 	} else {
 		return url, nil
 	}
 }
 
-func CreateUrl(urlId string, url string, expireAt time.Time) error {
-	if !CheckUrl(urlId) {
-		return fmt.Errorf("url exists.")
+func (r *urlRepository) Create(urlId string, url string, expireAt time.Time) error {
+	if !r.IsUrlIdExist(urlId) {
+		return errors.New("url exists")
 	}
 	newUrl := model.Urls{
 		UrlId:    urlId,
 		Url:      url,
 		ExpireAt: expireAt,
 	}
-	insertErr := pg.Db.Model(&model.Urls{}).Create(&newUrl).Error
+	insertErr := r.db.Model(&model.Urls{}).Create(&newUrl).Error
 	return insertErr
 }
 
-func CheckUrl(urlId string) bool {
+func (r *urlRepository) IsUrlIdExist(urlId string) bool {
 	var url model.Urls
 
-	if err := pg.Db.Select(fields).Where("url_id=?", urlId).First(&url); err.RowsAffected == 0 {
+	if err := r.db.Select(fields).Where("url_id=?", urlId).First(&url); err.RowsAffected == 0 {
 		return true
 	}
 	return false
