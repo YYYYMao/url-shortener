be3c86f31f571011c65fdf8f432dba334c3268bd
Ying Mao <ying.mao@wemoscooter.com>
2023-01-02 05:17:51 +0800

    feat: modify setuprouter

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 	"urlshortener/api/redirect"
 	"urlshortener/api/urls"
+	repo "urlshortener/repositories"
+	"urlshortener/utils/pg"
+	"urlshortener/utils/redis"
 
 	_ "urlshortener/docs"
 
@@ -24,17 +35,57 @@ func init() {
 // @description Url Shortener API.
 // @host localhost:8080
 func main() {
+	app := setupRouter()
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: app,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+
+}
+
+func setupRouter() *gin.Engine {
 	app := gin.Default()
 	if mode := gin.Mode(); mode == gin.DebugMode {
 		url := ginSwagger.URL(fmt.Sprintf("http://localhost:%d/swagger/doc.json", 8080))
 		app.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 	}
 	app.Use(gin.Recovery())
-	redirect.RouteUrls(app)
-	urls.RouteUrls(app)
 
-	err := app.Run(":8080")
-	if err != nil {
-		panic(err)
+	urlRepo := repo.NewUrlRepository(pg.Db)
+	urlService := urls.NewUrlService(urlRepo)
+	cacheRepo := redis.NewRedisRepository(redis.Client)
+	redirectService := redirect.NewRedirectService(urlRepo, cacheRepo)
+
+	redirectController := &redirect.RedirectController{
+		RedirectService: redirectService,
 	}
+
+	urlController := &urls.UrlController{
+		UrlService: urlService,
+	}
+
+	redirect.RouteUrls(app, redirectController)
+	urls.RouteUrls(app, urlController)
+
+	return app
 }
